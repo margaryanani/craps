d635444ec8c4bc9d89aa655326874215d535bdbe
Ani Margaryan <ani.m.yan55@gmail.com>
2024-08-24 16:39:15 +0400

    Updated, used classes and functions

--- craps.py
+++ craps.py
@@ -1,6 +1,65 @@
 import random
 
-input("""
+
+class Craps:
+    def __init__(self):
+        self.balance = 100
+        self.game_over = False
+        self.goal = None
+
+    def dice_roll(self):
+        dice_1 = random.randint(1, 6)
+        dice_2 = random.randint(1, 6)
+        roll = dice_1 + dice_2
+        print(f"{roll} = {dice_1} + {dice_2}")
+        return roll
+
+    def place_bet(self):
+        while True:
+            try:
+                bet = int(input("Place your bet: "))
+                if bet > self.balance:
+                    print("You don't have enough money.")
+                elif bet <= 0:
+                    print("You must place a positive bet.")
+                else:
+                    return bet
+            except ValueError:
+                print("Invalid input. Enter a valid number.")
+
+    def game(self):
+        while not self.game_over:
+            bet = self.place_bet()
+            roll = self.dice_roll()
+
+            if roll in [7, 11]:
+                self.balance += bet
+                print("You win!")
+                self.game_over = True
+            elif roll in [2, 3, 12]:
+                self.balance -= bet
+                print("You lose!")
+                self.game_over = True
+            elif roll in [4, 5, 6, 8, 9, 10]:
+                self.goal = roll
+                print(f"Your goal is now {self.goal}.")
+                self.roll_till_goal_or_seven(bet)
+
+    def roll_till_goal_or_seven(self, bet):
+        while not self.game_over:
+            input('Press enter to roll again. ')
+            roll = self.dice_roll()
+            if roll == self.goal:
+                self.balance += bet
+                print(f"You rolled your goal number {self.goal} and win!")
+                self.game_over = True
+            elif roll == 7:
+                self.balance -= bet
+                print("You rolled a 7 and lose!")
+                self.game_over = True
+
+    def play(self):
+        input("""
 WELCOME TO CRAPS!
 
 The rules of the game:
@@ -12,65 +71,22 @@ casino wins. If during the first roll the sum is 4, 5, 6, 8,
 should roll the dice till they roll the goal number again. If the 
 player rolls a 7 before rolling the goal number, they lose.
 
-Your balance will be 100$. Before rolling the dice you'll make a bet.
+Your balance will be $100. Before rolling the dice you'll make a bet.
 
 Press Enter to start the game.
 """)
+        while self.balance > 0:
+            self.game_over = False
+            self.game()
+            if self.balance > 0:
+                print(f"Your current balance is ${self.balance}.")
+                if input("Would you like to play again? (y/n): ").lower() != 'y':
+                    break
+            else:
+                print("You don't have enough money.")
 
-balance = 100
 
-game = True
-while game:
-    dice_1 = random.randint(1, 6)
-    dice_2 = random.randint(1, 6)
-    sum_1 = dice_1 + dice_2
-    bet = int(input("How much would you like to bet? "))
-    while bet > balance:
-        bet = int(input(f"Your balance is {balance}$. You can bet {balance}$ or less. Try again.  "))
-        if bet <= balance:
-            input('Press Enter to roll the dice. ')
-            input(f'{dice_1} + {dice_2} = {sum_1}')
-            if sum_1 in [7, 11]:
-                print('You win!')
-                balance = balance + bet
-                print(f'Your balance is {balance}$')
-            elif sum_1 in [2, 3, 12]:
-                print('Craps. You lost!')
-                balance = balance - bet
-                print(f'Your balance is {balance}$')
-            elif sum_1 in [4, 5, 6, 8, 9, 10]:
-                print("Goal!")
-                dice_1 = random.randint(1, 6)
-                dice_2 = random.randint(1, 6)
-                sum_2 = dice_1 + dice_2
-                print(f'{dice_1} + {dice_2} = {sum_2}')
-                while sum_2 != 7:
-                    dice_1 = random.randint(1, 6)
-                    dice_2 = random.randint(1, 6)
-                    sum_2 = dice_1 + dice_2
-                    input(f'{dice_1} + {dice_2} = {sum_2}')
-                    if sum_2 == 7:
-                        print('You lose!')
-                        balance = balance - bet
-                        print(f'Your balance is {balance}$')
-                        break
-                    elif sum_2 == sum_1:
-                        print('You win!')
-                        balance = balance + bet
-                        print(f'Your balance is {balance}$')
-                        break
-            if balance <= 0:
-                print("You don't have any money to continue.")
-                game = False
-            elif balance > 0:
-                print('do you want to play again?')
-                answer = input("If you want to continue Enter 'yes', if not, enter 'no'> ")
-                if answer == 'yes':
-                    game = True
-                elif answer == 'no':
-                    print('Thank you for playing!')
-                    print(f'Your total balance is {balance}$')
-                    game = False
-                elif balance <= 0:
-                    print("You don't have any money to continue.")
-                    game = False
+if __name__ == "__main__":
+    game = Craps()
+    game.play()
+
